0a85cf745126fd8af941b31aad288d66ee9a6860
phenriqued <pedrodias3737@gmail.com>
2025-01-15 17:27:52 -0300

    Hello World

--- src/main/java/com/RESTful_API/BirdRed/Controllers/HelloWorld/HelloWorld.java
+++ src/main/java/com/RESTful_API/BirdRed/Controllers/HelloWorld/HelloWorld.java
@@ -1,2 +1,20 @@
-package com.RESTful_API.BirdRed.Controllers.HelloWorld;public class HelloWorld {
+package com.RESTful_API.BirdRed.Controllers.HelloWorld;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/HelloBird")
+public class HelloWorld {
+
+
+    @GetMapping
+    public ResponseEntity<String> helloWorld(){
+        return ResponseEntity.ok().body("Hello \n \uD83D\uDC26");
+    }
+
+
 }
